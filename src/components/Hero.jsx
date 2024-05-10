c465bd9195563275b9bcd357b4fefdcefaec6133
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-05-10 16:57:21 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,9 +28,7 @@ const Hero = () => {
           Tutoring.
         </h1>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-          Our team of experts uses a methodology to identify the credit cards
-          most likely to fit your needs. We examine annual percentage rates,
-          annual fees.
+          Master organic chemistry with personalized tutoring sessions tailored to your needs. Our experts cover everything from reaction mechanisms, stereochemistry to spectroscopy, ensuring your success in the subject and deeper understanding. Learning, not memorizing! 
         </p>
       </div>
 
