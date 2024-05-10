e601559e35d6ce5cefbc255ae084afa57299982a
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-05-10 17:28:38 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from "../style";
-import { discount, robot } from "../assets";
+import { discount, chemistry } from "../assets";
 import GetStarted from "./GetStarted";
 
 const Hero = () => {
@@ -33,7 +33,7 @@ const Hero = () => {
       </div>
 
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+        <img src={chemistry} alt="billing" className="w-[95%] h-[78%] rounded-xl relative z-[5] mr-5" />
 
         {/* gradient start */}
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
