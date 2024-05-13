c431bb15d49b14b29d90d114c10f2ab96296e6ac
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-05-12 21:45:51 -0400

    Update Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -12,8 +12,7 @@ const Testimonials = () => (
       </h2>
       <div className="w-full md:mt-0 mt-6">
         <p className={`${styles.paragraph} text-left max-w-[450px]`}>
-          Everything you need to accept card payments and grow your business
-          anywhere on the planet.
+          Unlock the door to academic success with our comprehensive tutoring services tailored to meet your learning needs.
         </p>
       </div>
     </div>
