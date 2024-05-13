28558130371c62d50a114649c1896cab515c1aa8
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-05-13 08:43:38 -0400

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, brain, giftBox, clock  } from "../assets";
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, brain, giftBox, clock  } from "../assets";
 
 export const navLinks = [
   {
