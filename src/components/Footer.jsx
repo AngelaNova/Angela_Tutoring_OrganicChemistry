189d3083950980a0ad5c0b7c928ddc817683d532
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-05-13 09:50:51 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,10 @@ import styles from "../style";
 import { logo } from "../assets";
 import { footerLinks, socialMedia } from "../constants";
 
-const Footer = () => (
+const Footer = () => {
+  const d = new Date();
+  const year = d.getFullYear();
+  return(
   <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
     <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
       <div className="flex-[1] flex flex-col justify-start mr-10">
@@ -38,8 +41,8 @@ const Footer = () => (
     </div>
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
-      <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-        Copyright Ⓒ 2024 Angela Novakovic - Tutoring Services - Organic Chemistry. All Rights Reserved.
+      <p className="font-poppins font-normal text-center text-[12px] leading-[27px] text-white text-opacity-85">
+        Copyright Ⓒ {year} Angela Novakovic - Tutoring Services - Organic Chemistry. All Rights Reserved.
       </p>
 
       <div className="flex flex-row md:mt-0 mt-6">
@@ -57,6 +60,7 @@ const Footer = () => (
       </div>
     </div>
   </section>
-);
+  )
+};
 
 export default Footer;
