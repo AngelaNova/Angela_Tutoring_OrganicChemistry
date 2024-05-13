2d6e3eaa3dd74ab445421be4f160c1af31b01214
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-05-13 16:31:03 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,8 +6,8 @@ const Hero = () => {
   return (
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6 `}>
-        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-[-50px]">
-          <img src={discount} alt="discount" className="w-[32px] h-[32px] animate-[spin_6s_linear_infinite]" />
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-[-10%]">
+          <img src={discount} alt="discount" className="w-[8%] h-[8%] animate-[spin_6s_linear_infinite]" />
           <p className={`${styles.paragraph} ml-2 `}>
             <span className="text-white">10%</span> Discount For{" "}
             <span className="text-white">12 Month</span> Account
@@ -15,23 +15,23 @@ const Hero = () => {
         </div>
 
         <div className="flex flex-row justify-between items-center w-full ">
-          <h1 className="flex-1 font-poppins font-semibold md:text-[32px] lg:text-[42px] text-white ss:leading-[100.8px] leading-[75px] ">
+          <h1 className="flex-1 font-poppins font-semibold  text-[200%] text-white leading-[75px]  ">
             This is 
-            <span className="text-gradient ss:text-[42px]"> Organic Chemistry </span> 
+            <span className="text-gradient"> Organic <br/> Chemistry </span> 
             Tutoring.
           </h1>
-          <div className="ss:flex hidden mb-[300px] md:mr-4  ">
+          <div className="ss:flex hidden mb-[25%] mr-[42%] ">
             <GetStarted />
           </div>
         </div>
 
-        <p className={`${styles.paragraph} max-w-[470px] text-justify mt-[-40px]`}>
+        <p className={`${styles.paragraph} max-w-[470px] text-justify mt-[-10%]`}>
           Master organic chemistry with personalized tutoring sessions tailored to your needs. Our experts cover everything from reaction mechanisms to spectroscopy, ensuring your success in the subject and deeper understanding. Learning, not memorizing! 
         </p>
       </div>
 
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-        <img src={chemistry} alt="billing" className="w-[95%] h-[78%] rounded relative z-[5] mr-0 ml-[-30px]" />
+        <img src={chemistry} alt="billing" className="w-[30%] h-[20%] rounded relative z-[5] mt-[-32%] mr-0 ml-[55%]" />
 
         {/* gradient start */}
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
