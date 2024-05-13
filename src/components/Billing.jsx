1e672f06ed76aed0b79db003ddad0827f1fad367
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-05-12 21:19:06 -0400

    Update Billing.jsx

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,4 +1,4 @@
-import { apple, bill, google } from "../assets";
+import { bill, McGill } from "../assets";
 import styles, { layout } from "../style";
 
 const Billing = () => (
@@ -14,18 +14,15 @@ const Billing = () => (
 
     <div className={layout.sectionInfo}>
       <h2 className={styles.heading2}>
-        Easily control your <br className="sm:block hidden" /> billing &
-        invoicing
+      Manage your <br className="sm:block hidden" /> Chemistry Learning & Progress
+ 
       </h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
-        aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
-        placerat.
+        Dive into the world of organic chemistry alongside a McGill University alumna. Our tutoring services offer unparalleled expertise to help you excel in your chemistry journey.
       </p>
 
       <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
-        <img src={apple} alt="google_play" className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer" />
-        <img src={google} alt="google_play" className="w-[144.17px] h-[43.08px] object-contain cursor-pointer" />
+        <img src={McGill} alt="McGill University" className="rounded-lg w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer opacity-94" />
       </div>
     </div>
   </section>
