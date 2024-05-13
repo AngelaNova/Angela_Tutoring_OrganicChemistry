4af45035cd21b13368082ef54b784d52fa8d580a
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-05-13 17:18:38 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,9 @@ import GetStarted from "./GetStarted";
 const Hero = () => {
   return (
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6 `}>
-        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-[-10%]">
-          <img src={discount} alt="discount" className=" md:h-[35%] sm:h-[8%]   animate-[spin_6s_linear_infinite]" />
+      <div className={`flex-1 ${styles.flexStart} flex-col lg:px-0 sm:px-16 px-6 `}>
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+          <img src={discount} alt="discount" className="  animate-[spin_6s_linear_infinite] w-[32px] h-[32px]" />
           <p className={`${styles.paragraph} ml-2 `}>
             <span className="text-white">10%</span> Discount For{" "}
             <span className="text-white">12 Month</span> Account
@@ -20,9 +20,6 @@ const Hero = () => {
             <span className="text-gradient"> Organic <br/> Chemistry </span> 
             Tutoring.
           </h1>
-          <div className="ss:flex hidden mb-[20%] mr-[42%] mt-[5%]">
-            <GetStarted />
-          </div>
         </div>
 
         <p className={`${styles.paragraph} max-w-[470px] text-justify mt-[-10%]`}>
@@ -38,10 +35,6 @@ const Hero = () => {
         <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 green__gradient" />
         {/* gradient end */}
-      </div>
-
-      <div className={`ss:hidden ${styles.flexCenter}`}>
-        <GetStarted/>
       </div>
     </section>
   );
