e51f2027fc766e2346174bcb66e6dfa538d9d7be
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-05-12 21:30:50 -0400

    Update Billing.jsx

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,10 +1,10 @@
-import { bill, McGill } from "../assets";
+import { lab, McGill } from "../assets";
 import styles, { layout } from "../style";
 
 const Billing = () => (
   <section id="product" className={layout.sectionReverse}>
     <div className={layout.sectionImgReverse}>
-      <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+      <img src={lab} alt="billing" className="w-[100%] h-[100%] relative z-[5] rounded ml-[-60px]" />
 
       {/* gradient start */}
       <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
