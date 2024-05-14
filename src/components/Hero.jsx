c934983def7ee5784af5a72e7e3d20fcca1c61ba
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-05-14 10:28:13 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
       <div className={`flex-1 ${styles.flexStart} flex-col lg:px-0 sm:px-16 px-6 `}>
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
-          <img src={discount} alt="discount" className="  animate-[spin_6s_linear_infinite] w-[32px] h-[32px]" />
+          <img src={discount} alt="discount" className="discount_img animate-[spin_6s_linear_infinite]" />
           <p className={`${styles.paragraph} ml-2 `}>
             <span className="text-white">10%</span> Discount For{" "}
             <span className="text-white">12 Month</span> Account
@@ -27,7 +27,7 @@ const Hero = () => {
       </div>
 
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-        <img src={chemistry} alt="chemistry" className="w-[30%] h-[20%] rounded relative z-[5] mt-[-37.9%] mr-0 ml-[55%]" />
+        <img src={chemistry} alt="chemistry" className="chemistry_img" />
 
         {/* gradient start */}
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
