227df97bbe503f70396ad3e3e52fd67c729fdba5
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-05-14 13:27:38 -0400

    Update Business.jsx

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -3,8 +3,8 @@ import styles, { layout } from "../style";
 import Button from "./Button";
 
 const FeatureCard = ({ icon, title, content, index }) => (
-  <div className={`flex flex-row p-6 rounded-[20px] max-w-[100%] ${index !== features.length - 1 ? "mb-6" : "mb-0"} `}>
-    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimGreen`}>
+  <div className={`flex flex-row p-6 rounded-[20px] max-w-[60%]  ${index !== features.length - 1 ? "mb-6" : "mb-0"} `}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimGreen  `}>
       <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
     </div>
     <div className="flex-1 flex flex-col ml-3">
@@ -24,7 +24,7 @@ const Business = () =>  (
       <h2 className={styles.heading2}>
         Chemistry Tutoring Excellence
       </h2>
-      <ul className={`${styles.paragraph} max-w-[470px] mt-5 list-disc list-inside text-justify`}>
+      <ul className={`${styles.paragraph} max-w-[500px] mt-5 list-disc list-inside text-justify`}>
         <b>You Learn, We Guide</b> <br/>
         <li>Achieve success in organic chemistry with our tailored tutoring services. Let us help you master the material and excel in your studies!</li>
         <li>With personalized tutoring, you can strengthen your understanding, improve your grades, and unlock your full potential in organic chemistry.</li>
