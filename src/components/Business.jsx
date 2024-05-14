d9ed1900198efb0f85c29eb39c872ecac56df2dc
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-05-14 11:03:56 -0400

    Update Business.jsx

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -3,15 +3,15 @@ import styles, { layout } from "../style";
 import Button from "./Button";
 
 const FeatureCard = ({ icon, title, content, index }) => (
-  <div className={`flex flex-row p-6 rounded-[20px]  ${index !== features.length - 1 ? "mb-6" : "mb-0"} `}>
-    <div className={`w-[50px] h-[50px] rounded-full ${styles.flexCenter} bg-dimGreen`}>
-      <img src={icon} alt="star" className="w-[50%] object-contain" />
+  <div className={`flex flex-row p-6 rounded-[20px] max-w-[600px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} `}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimGreen`}>
+      <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
     </div>
     <div className="flex-1 flex flex-col ml-3">
       <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
         {title}
       </h4>
-      <p className="font-poppins font-normal text-dimWhite text-[16px] max-w-[500px]">
+      <p className="font-poppins font-normal text-dimWhite text-[16px]">
         {content}
       </p>
     </div>
@@ -22,7 +22,7 @@ const Business = () =>  (
   <section id="features" className={layout.section}>
     <div className={layout.sectionInfo}>
       <h2 className={styles.heading2}>
-      Chemistry Tutoring Excellence
+        Chemistry Tutoring Excellence
       </h2>
       <ul className={`${styles.paragraph} max-w-[470px] mt-5 list-disc list-inside text-justify`}>
         <b>You Learn, We Guide</b> <br/>
@@ -33,7 +33,7 @@ const Business = () =>  (
       <Button styles={`mt-10`} />
     </div>
 
-    <div className={`${layout.sectionImg} items-start flex-col mt-[-31%] mr-[-52%] `}>
+    <div className={`${layout.sectionImg} flex-col flex-wrap `}>
       {features.map((feature, index) => (
         <FeatureCard key={feature.id} {...feature} index={index} />
       ))}
