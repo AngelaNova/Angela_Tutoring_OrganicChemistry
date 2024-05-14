fe7d22a58573df3f0dac6b99cb2c81275a7ca2cb
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-05-13 21:44:44 -0400

    Update Billing.jsx

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -4,7 +4,7 @@ import styles, { layout } from "../style";
 const Billing = () => (
   <section id="License" className={layout.sectionReverse}>
     <div className={layout.sectionImgReverse}>
-      <img src={lab} alt="billing" className="w-[60%] h-[60%] md:w-[70%] md:h-[80%] relative z-[5] rounded md:ml-[-60px]" />
+      <img src={lab} alt="billing" className="w-[40%] h-[40%] mr-[-60%] md:mr-0 mt-[-38%] md:mt-0 md:w-[70%] md:h-[80%] relative z-[5] rounded md:ml-[-60px]" />
 
       {/* gradient start */}
       <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
@@ -14,8 +14,7 @@ const Billing = () => (
 
     <div className={layout.sectionInfo}>
       <h2 className={styles.heading2}>
-      Manage your <br className="sm:block hidden" /> Chemistry Learning & Progress
- 
+      Manage your Chemistry <br className="sm:block hidden" /> Learning & Progress
       </h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
         Dive into the world of organic chemistry alongside a McGill University alumna. Our tutoring services offer unparalleled expertise to help you excel in your chemistry journey.
