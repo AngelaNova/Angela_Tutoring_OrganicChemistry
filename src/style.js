51bc68c9851f671d7d86b7902e1bcd51594a8380
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-05-13 21:44:42 -0400

    Update style.js

--- src/style.js
+++ src/style.js
@@ -22,7 +22,7 @@ export const layout = {
   sectionImgReverse: `flex-1 flex ${styles.flexCenter} md:mr-10 mr-0 md:mt-0 mt-10 relative `,
   sectionImg: ` flex ${styles.flexCenter} md:ml-10 ml-0 md:mt-0 mt-10 relative`,
 
-  sectionInfo: `flex-1 ${styles.flexStart} flex-col`,
+  sectionInfo: `flex flex-1 ${styles.flexStart} flex-col`,
 };
 
 export default styles;
