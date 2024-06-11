efcb80ce74dea03d541922ba478f00a3cf6ec703
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-06-11 15:57:47 -0400

    Update studentReducer.js

--- src/redux/reducers/studentReducer.js
+++ src/redux/reducers/studentReducer.js
@@ -1,8 +1,7 @@
-import { ADD_STUDENT, EDIT_STUDENT, DELETE_STUDENT, FILTER_STUDENTS } from '../actions/studentActions';
+import { ADD_STUDENT, EDIT_STUDENT, DELETE_STUDENT, ID_FOR_STUDENT } from '../actions/studentActions';
 
 const initialState = {
-  allStudents: [{"name": "studentName", "lastName": "StudentLastName", "username":"Student01","password":"unknown", "subject":"organic"},{"name": "studentName", "lastName": "StudentLastName", "username":"Student02","password":"unknown", "subject":"all"}],
-  filteredStudents: [],
+  allStudents: [{"name": "studentName", "lastName": "StudentLastName", "username":"Student01","password":"unknown", "subject":"organic"},{"name": "studentName", "lastName": "StudentLastName", "username":"Student02","password":"unknown", "subject":"all"}]
 };
 
 const studentReducer = (state = initialState, action) => {
@@ -25,8 +24,9 @@ const studentReducer = (state = initialState, action) => {
         ...state,
         allStudents: state.allStudents.filter((student) => student.id !== action.payload),
       };
-    case FILTER_STUDENTS:
+    case ID_FOR_STUDENT:
       return {
+        //to change TODO
         ...state,
         filteredStudents: state.allStudents.filter((student) => student.subject === action.payload),
       };
